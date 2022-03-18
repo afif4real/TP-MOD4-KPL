9faddd5067e27f289bd1bad60ddfe6520ec767cd
afif4real <af.rayhan@gmail.com>
2022-03-18 17:19:20 +0700

    menambahkan class HaloGeneric dengan method SapaUser

---
+++ src/HaloGeneric.java
@@ -0,0 +1,9 @@
+public class HaloGeneric {
+    public static void SapaUser(String name) {
+        System.out.printf("Halo user %s",name);
+    }
+
+    public static void main(String[] args) {
+        SapaUser("afif4real");
+    }
+}
